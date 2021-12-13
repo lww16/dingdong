8428dab722b876ffd6ec9541ea65bfe5e004a32c
hp-lww <1535964157@qq.com>
2021-12-13 08:52:55 +0800

    first commit

---
+++ friend_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin #Django自动在admin.py文件中导入
+#from friend_api.models import User,Article #这个需要我们自己导入相应的模型类（数据表
+#admin.site.register([User,Article])
+# Register your models here.
---
+++ friend_api/views.py
@@ -0,0 +1,9 @@
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class loginView(APIView):
+    def post(self,request,*args,**kwargs):
+        print(request.data)
+        return Response({"status":True})
